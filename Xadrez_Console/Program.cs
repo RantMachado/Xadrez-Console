5a6eabe95114a64fbf98b8e5a84587f44391e9c5
Randolfo Machado <talktorandolph@live.com>
2019-06-29 02:26:27 -0300

    Tratando erros com excessao personalizada

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -2,6 +2,7 @@
 using Xadrez_Console.tabuleiro;
 using Xadrez_Console.Xadrez;
 using Xadrez_Console.tabuleiro.Enums;
+using Xadrez_Console.tabuleiro.Exceptions;
 
 namespace Xadrez_Console
 {
@@ -9,16 +10,22 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
 
-            Tela.ImprimirTabuleiro(tabuleiro);
+                Tela.ImprimirTabuleiro(tabuleiro);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
-
         }
     }
 }
---
+++ Xadrez_Console/Tabuleiro/Exceptions/TabuleiroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Xadrez_Console.tabuleiro.Exceptions
+{
+    class TabuleiroException : ApplicationException
+    {
+        public TabuleiroException(string Message) : base(Message)
+        {
+        }
+    }
+}
