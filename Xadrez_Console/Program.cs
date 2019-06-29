abd72b0623b5607edd47253a942a59899cfb42e6
Randolfo Machado <talktorandolph@live.com>
2019-06-29 02:40:07 -0300

    Criando a classe posicao xadrez

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -10,20 +10,9 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
-
-                Tela.ImprimirTabuleiro(tabuleiro);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.ToPosicao());
 
             Console.ReadLine();
         }
---
+++ Xadrez_Console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,28 @@
+﻿using Xadrez_Console.tabuleiro;
+
+namespace Xadrez_Console.Xadrez
+{
+    class PosicaoXadrez
+    {
+        //Auto-Properties
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        //Construtores
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
